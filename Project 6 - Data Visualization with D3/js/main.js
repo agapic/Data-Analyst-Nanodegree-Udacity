929c3b80a4352fab56562f427faf1f91bf548ac4
agapic <andrewjdg@gmail.com>
2016-04-18 20:28:22 -0400

    Version 2

--- Project 6 - Data Visualization with D3/js/main.js
+++ Project 6 - Data Visualization with D3/js/main.js
@@ -18,21 +18,27 @@ function begin(data) {
               return o.time;
           })
 
+          var latestYear = _.maxBy(sprinters, function(o) {
+          	   return o.year;
+          })
+          var earliestYear = _.minBy(sprinters, function(o) {
+          	   return o.year;
+          })
+
           var best_time = firstPlaceRunner.time;
           var worst_time = lastPlaceRunner.time;
+          var yearDifference = latestYear.year - earliestYear.year;
 
           // If the person is last is really really far behind, decrease the leftmost boundary by 15.
-          if ((distance - (distance / worst_time) * best_time) > (distance - leftside)+10) {
-              leftside -= 15;
+          if ((distance - (distance / worst_time) * best_time) > (distance - leftside)) {
+              leftside = parseFloat(distance - (distance - (distance / worst_time) * best_time) - 3).toFixed(2);
+              if (leftside < 0) return;
           }
-          // Otherwise, decrease by 5 since the person is last is still _relatively_ close to the person in first.
-          else if ((distance - (distance / worst_time) * best_time) > (distance - leftside)) {
-              leftside -= 5;
-          }
+   
           // Maps speeds/positions to pixels on teh screen
           function get_x_pos(data) {
               // 
-              if (data.name == firstPlaceRunner.name) {
+              if (data.time == best_time) {
                   data.distance_from_first = 0;
                   data.pos_x = width + offset;
                   return width + offset;
@@ -50,36 +56,73 @@ function begin(data) {
               return x_coordinate + offset;
           }
 
+          function get_y_pos(data) {
+              if (data.year == latestYear.year) {
+                  data.pos_y = y_val_top;
+                  return y_val_top;
+              }
+              if (data.year == earliestYear.year) {
+              	  data.pos_y = y_val;
+              	  return y_val;
+              }
+              var pixelDiff = y_val - y_val_top;
+
+              var ratio = (latestYear.year-data.year) / yearDifference;
+              data.pos_y = y_val_top + ratio*pixelDiff;
+              return y_val_top + ratio*pixelDiff;
+          }
+
+
           // Begin drawing the canvas
           // Create the container
+          var y_val = 500.5;
+          var y_val_top = 100.5;
           var svg = d3.select("#line")
           .append("svg")
           .attr("width", width + 4*offset)
-          .attr("height", 400);
+          .attr("height", 600);
 
+         // Create the title
+          svg.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", 55)
+
+        .style("class", "title") 
+                .attr("text-anchor", "middle")   
+        .text("Track and Field World Record Distances and Times");
           // Create the line for sprinters to be plotted on
           svg.append("line")
           .attr("x1", offset)
-          .attr("y1", 200.5)
+          .attr("y1", y_val)
           .attr("x2", width + offset)
-          .attr("y2", 200.5)
+          .attr("y2", y_val)
+          .attr("stroke", "black");
+
+          svg.append("line")
+          .attr("x1", width + offset)
+          .attr("y1", y_val)
+          .attr("x2", width + offset)
+          .attr("y2", y_val_top)
           .attr("stroke", "black");
 
           // Create the rightmost distance (e.g 100m, 200m)
           svg.append("text")
           .text(distance + "m")
           .attr("x", width + offset)
-          .attr("y", 240)
+          .attr("y", y_val + 39.5)
           .attr("class", "small")
           .attr("text-anchor", "middle");
 
           // Create the leftmost distance (this changes when custom runners are entered into the race)
           svg.append("text")
-          .text(leftside + "m")
+          .text(Math.round(leftside) + "m")
           .attr("x", 20)
-          .attr("y", 240)
+          .attr("y", y_val + 39.5)
           .attr("class", "small")
           .attr("text-anchor", "middle");
+
+          
+
 
           // Create the circles based on the data.
           svg.selectAll("sprinters")
@@ -87,10 +130,10 @@ function begin(data) {
           .enter().append("circle")
           .attr("r", 5)
           .attr("cx", 30)
-          .attr("cy", 200)
+          .attr("cy", y_val - 0.5)
           .attr("fill", function(d) {
               // Make the person in first place distinct from the others.
-              return d.name == firstPlaceRunner.name ? "#c00" : "#333"
+              return d.time == best_time ? "#c00" : "#333"
           })
           
           .on("mouseover", function(d) { svg.select("g#y_" + d.year).style("display", ""); })
@@ -99,33 +142,35 @@ function begin(data) {
           .ease("linear")
           .duration(1000)
           .attr("cx", function(d) { return get_x_pos(d); })
+          .attr("cy", function(d) { return get_y_pos(d); })
+
+
           .each("end",
                 function(d) {
                     var grp = svg.append("svg:g").attr("id", "y_" + d.year).style("display", "none");
-                    console.log(grp);
                     // Displays name, year, and time when hovered over.
                     grp.append("text")
                         .text(d.name + ' (' + d.year + ') - ' + d.time + 's')
-                        .attr("y", 40)
-                        .attr("x", d.pos_x)
+                        .attr("y", d.pos_y - 10)
+                        .attr("x", d.pos_x - 30)
                         .attr("text-anchor", "middle")
                         .attr("class", "small");
                     // Create vertical line that extends to the runner's description
                     grp.append("line")
                         .attr("x1", Math.round(d.pos_x) + 0.5)
-                        .attr("y1", 45)
+                        .attr("y1", d.pos_y)
                         .attr("x2", Math.round(d.pos_x) + 0.5)
-                        .attr("y2", 194)
+                        .attr("y2", y_val - 6.5)
                         .attr("stroke", "#ccc")
                         .attr("stroke-width", 1)
                         .attr("opacity","0.5")
 
                     // Initial transition finished
                     // Make runner in first distinct from the rest.
-                    if (d.name == firstPlaceRunner.name) {
+                    if (d.time == best_time) {
                         svg.append("circle")
                             .attr("cx", width + offset)
-                            .attr("cy", 200)
+                            .attr("cy", y_val - 0.5)
                             .attr("fill", "none")
                             .attr("stroke-width", 2).attr("stroke", "#cc0000")     
                     }
@@ -133,38 +178,55 @@ function begin(data) {
                         // Vertical line below the main line for connecting to the person in first.
                         grp.append("line")
                             .attr("x1", Math.round(d.pos_x) + 0.5)
-                            .attr("y1", 206)
+                            .attr("y1", y_val + 5.5)
                             .attr("x2", Math.round(d.pos_x) + 0.5)
-                            .attr("y2", 225)
+                            .attr("y2", y_val + 24.5)
                             .attr("stroke", "#ccc")
                             .attr("opacity","0.5")
                         // Horizontal line below the main line for connecting to the person in first.
                         grp.append("line")
                             .attr("x1", Math.round(d.pos_x))
-                            .attr("y1", 224.5)
+                            .attr("y1", y_val + 24)
                             .attr("x2", width + offset)
-                            .attr("y2", 224.5)
+                            .attr("y2", y_val + 24)
+                            .attr("stroke", "#ccc")
+                            .attr("opacity","0.5")
+                        // Horizontal line that links to the year
+                        grp.append("line")
+                            .attr("x1", Math.round(d.pos_x))
+                            .attr("y1", d.pos_y)
+                            .attr("x2", width + offset)
+                            .attr("y2", d.pos_y)
                             .attr("stroke", "#ccc")
                             .attr("opacity","0.5")
                         //Vertical line that connects the previous horizontal line to the person in first.
                         grp.append("line")
                             .attr("x1", width + offset + 0.5)
-                            .attr("y1", 206)
+                            .attr("y1", y_val + 5.5)
                             .attr("x2", width + offset + 0.5)
-                            .attr("y2", 225)
+                            .attr("y2", y_val + 24.5)
                             .attr("stroke", "#ccc")
                             .attr("opacity","0.5")
                         // Displays the distance 
                         grp.append("text")
                             .text(Math.round(d.distance_from_first * 100) / 100 + ' m')
-                            .attr("y", 222)
+                            .attr("y", y_val + 21.5)
                             .attr("x", (d.pos_x + width + offset) / 2)
                             .attr("text-anchor", "middle")
                             .attr("class", "small");
                     }
-                }
-           );
-        }
+                });
+          
+    _.each(data[distance], function(o) {
+       svg.append("text")
+      .text(o.year)
+      .attr("x", width + offset + 17)
+      .attr("y", o.pos_y +2)
+      .attr("class", "tiny")
+      .attr("text-anchor", "middle");
+	})
+           
+}
 // Updates the current screen if user changes distance type (100m to 200m, for example)
 function update(dist) {
 	d3.select("svg").remove();
@@ -183,7 +245,7 @@ function addRunner() {
     var runner = element.value;
     numRunners++;
     customRunners.push({name: "Runner" + numRunners, 
-                      year: 2016 + numRunners, 
+                      year: 2012 - numRunners, 
                       time: +runner
                     });
     update(distance);
